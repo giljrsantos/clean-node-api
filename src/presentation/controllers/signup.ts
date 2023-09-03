8c7eefbd37848d3aaa370ac85930023b4b2f9c8e
Gilberto Jr <giljrsantos@gmail.com>
2023-09-03 13:46:45 -0300

    feat: correção eslint

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,5 @@
-/* eslint-disable @typescript-eslint/space-before-function-paren */
-/* eslint-disable @typescript-eslint/indent */
 /* eslint-disable padded-blocks */
 export class SignUpController {
-    // eslint-disable-next-line @typescript-eslint/indent
-    /* eslint-disable @typescript-eslint/indent */
     handle(httpRequest: any): any {
 
         if (!httpRequest.body.name) {
