4a5963353959e42bbbec5226918a7b489f2e2c79
Gilberto Jr <giljrsantos@gmail.com>
2023-09-03 13:00:01 -0300

    feat: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/space-before-function-paren */
+/* eslint-disable @typescript-eslint/indent */
+/* eslint-disable padded-blocks */
+export class SignUpController {
+    // eslint-disable-next-line @typescript-eslint/indent
+    /* eslint-disable @typescript-eslint/indent */
+    handle(httpRequest: any): any {
+        return {
+            statusCode: 400
+        }
+    }
+}
